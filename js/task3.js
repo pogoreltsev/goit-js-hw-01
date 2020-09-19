b7747911d887cd3f0c04eae19cc59fd55b1e3228
pogoreltsev <stoyy15@gmail.com>
2020-09-19 22:46:36 +0300

    task3

---
+++ js/task3.js
@@ -0,0 +1,19 @@
+const userPassword = 'jqueryismyjam';
+
+const ADMIN_PASSWORD = 'jqueryismyjam';
+
+const CANCELED_BY_USER = 'Отменено пользователем!';
+const ACCESS_IS_ALLOWED = 'Добро пожаловать!';
+const ACCESS_DENIED = 'Доступ запрещен, неверный пароль!';
+let message;
+
+// Обьявляю инструкцию
+if (userPassword === ADMIN_PASSWORD) {
+  message = ACCESS_IS_ALLOWED;
+} else if (userPassword === null) {
+  message = CANCELED_BY_USER;
+} else {
+  message = ACCESS_DENIED;
+}
+
+console.log(message);
